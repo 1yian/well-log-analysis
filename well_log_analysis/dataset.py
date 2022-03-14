383370dbabe2a1e65830b167b8d17d40de55dff0
yian <yian@utexas.edu>
2022-03-14 14:17:40 -0500

    update

---
+++ well_log_analysis/dataset.py
@@ -0,0 +1,41 @@
+import lasio
+import torch
+import sys
+import numpy as np
+
+class WellDataset(torch.utils.data.Dataset):
+
+    def __init__(self, path, return_sites=[], clean_sites=[], sequence_length=1):
+        self.df = lasio.read(path).df().reset_index()[[site for site in return_sites]].dropna(subset=clean_sites)
+        self.return_sites = return_sites
+        self.sequence_length = sequence_length
+        
+        self.map = {}
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if idx not in self.map:
+            ret = np.array([self.df.iloc[max(idx, 0)][self.return_sites] for idx in range(idx - self.sequence_length + 1, idx + 1)])
+            self.map[idx] = ret
+        return self.map[idx]
+
+    def __add_(self, other_dataset):
+
+        if type(other_dataset) != type(self):
+            raise TypeError("Must be added with another WellDataset")
+
+        if set(other_dataset.return_sites) != set(self.return_sites):
+            raise TypeError("Datasets must have the same return sites")
+
+        new_dataset = self.copy()
+        new_dataset.df = pandas.concat(self.df, other_dataset.df)
+        return new_dataset
+    
+    def mean(self):
+        
+        return np.mean(np.array(self.df[self.return_sites]), axis=0)
+    
+    def std(self):
+        return np.std(np.array(self.df[self.return_sites]), axis=0)
---
+++ well_log_analysis/models/mlp.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+
+    def __init__(self, input_features=10, hidden_layers=[64, 64, 64], output_features=20, sequence_length=1):
+        super().__init__()
+        layers = [input_features * sequence_length] + hidden_layers + [output_features]
+
+        self.fc_layers = nn.ModuleList()
+
+        self.activation = nn.SELU()
+
+        for i in range(1, len(layers)):
+            fc = nn.Linear(layers[i - 1], layers[i])
+            self.fc_layers.append(fc)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        for layer in self.fc_layers[:-1]:
+            x = nn.Dropout(0.4)(x)
+            x = layer(x)
+            x = self.activation(x)
+        x = self.fc_layers[-1](x)
+        return x
